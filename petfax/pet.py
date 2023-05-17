ded76c879b9109362ecac2740d09864ded683d74
James Aules <james_aules@icloud.com>
2023-05-17 14:43:16 -0400

    facts index

--- petfax/pet.py
+++ petfax/pet.py
@@ -8,15 +8,11 @@ print(pets)
 
 @bp.route('/')
 def index():
-    return render_template('index.html', pets=pets)
+    return render_template('pets/index.html', pets=pets)
 
 @bp.route('/show/<int:pet_id>')
 def show(pet_id): 
     pet = pets[pet_id - 1]
-    return render_template('show.html', pet=pet)
-
-@bp.route('/new')
-def new():
-    return render_template('new.html')
+    return render_template('pets/show.html', pet=pet)
 
 
