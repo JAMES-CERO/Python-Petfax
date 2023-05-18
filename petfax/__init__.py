b0b7b2997a29bdcf67b4d678851d60e9f8a5b362
James Aules <james_aules@icloud.com>
2023-05-18 19:05:06 -0400

    db postgres

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -4,6 +4,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:Bratva@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    from . import models
+    models.db.init_app(app)
+
+    
     @app. route('/')
     def hello():
        return 'Hello, PetFax'
@@ -14,5 +21,7 @@ def create_app():
 
     from . import fact
     app.register_blueprint(fact.bp)
+
+
         
     return app
